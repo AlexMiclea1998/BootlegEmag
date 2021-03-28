79edf180878e04ba406530f49bee5c2b226f9565
raluuusuciu <raluca_ioana_suciu@yahoo.com>
2021-03-28 21:53:19 +0300

    added events

---
+++ BootlegEmagService/Controllers/Events/Model/UserActionEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootlegEmagService.Events.Model
+{ 
+
+    public enum UserActionType
+    {
+        Login = 0,
+        Logout
+    }
+
+    public class UserActionEvent
+    {
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public UserActionType Type { get; set; }
+    }
+}
