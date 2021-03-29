9785ddc717a9bd9cb74af6f7385dfa612935b74d
Ajubalaki <horatiu.ciungan@yahoo.com>
2021-03-29 17:27:33 +0300

    added products

---
+++ BootlegEmagService/Product/ProductFacade.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BootlegEmagService.Product.Repository;
+
+namespace BootlegEmagService.Product
+{
+    public class ProductFacade
+    {
+        private ProductRepository ProductRepository {get; set;}
+
+        public ProductFacade(ProductRepository productRepository)
+        {
+            ProductRepository = productRepository;
+        }
+
+        public Models.Product createProduct(string name, string category, string price, string image)
+        {
+            return ProductRepository.createProduct( name,  category,  price,  image);
+        }
+
+        public Models.deleteProductDTO deleteProduct(string id, string name, string category, string price, string image)
+        {
+            return ProductRepository.deleteProduct(id, name, category, price, image);
+        }
+        public Models.updateProductDTO updateProduct(string id, string name, string category, string price, string image)
+        {
+            return ProductRepository.updateProduct(id, name, category, price, image);
+        }
+    }
+}
