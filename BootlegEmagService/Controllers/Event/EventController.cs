00c8a9462ef19906e7a4000f71ab505c711fc65f
zraul <zavaczki.raul@gmail.com>
2021-03-29 17:25:03 +0300

    add

---
+++ BootlegEmagService/Controllers/Event/EventController.cs
@@ -0,0 +1,47 @@
+﻿using BootlegEmagService.Controllers.Event.Http;
+using BootlegEmagService.Events;
+using BootlegEmagService.Events.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootlegEmagService.Controllers
+{
+    [Route("api/events")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private EventFacade Facade { get; set; }
+
+        public EventController()
+        {
+            Facade = new EventFacade();
+        }
+
+        [HttpGet("{userId}")]
+        public string Get(string userId)
+        {
+            return userId;
+        }
+
+        [Route("useraction")]
+        [HttpPost]
+        public IActionResult Post([FromBody] RegisterUserActionRequest userActionRequest)
+        {
+            var userAction = new UserActionEvent { Id = "0", UserId = userActionRequest.UserId, Type = userActionRequest.Type };
+            var result = Facade.RegisterUserAction(userAction);
+            var response = new RegisterUserActionResponse { HasDiscount = result };
+            return Ok(response);
+        }
+
+        [Route("productaction")]
+        [HttpPost]
+        public IActionResult Post([FromBody] ProductActionEvent productAction)
+        {
+            Facade.RegisterProductAction(productAction);
+            return Ok();
+        }
+    }
+}
---
+++ BootlegEmagService/Events/EventFacade.cs
@@ -0,0 +1,40 @@
+﻿using BootlegEmagService.Events.Model;
+using BootlegEmagService.Events.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootlegEmagService.Events
+{
+    public class EventFacade
+    {
+        private EventRepository Repository { get; set; }
+
+        public EventFacade()
+        {
+            Repository = new EventRepository();
+        }
+
+        public bool RegisterUserAction(UserActionEvent userAction)
+        {
+            Repository.RegisterUserAction(userAction);
+            return CheckForDiscount(userAction.UserId);
+        }
+
+        private bool CheckForDiscount(string UserId)
+        {
+            if (Repository.GetLoginActionCount(UserId) == 3)
+            {
+                Repository.RegisterDiscountForUser(UserId);
+                return true;
+            }
+            return false;
+        }
+
+        public void RegisterProductAction(ProductActionEvent productAction)
+        {
+            Repository.RegisterProductAction(productAction);
+        }
+    }
+}
