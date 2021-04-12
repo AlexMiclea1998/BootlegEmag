0a38bd3cbbe3de6b782b7e29ab081e8ccabe70ae
AlexMiclea1998 <alexmiclea@rocketmail.com>
2021-04-12 20:30:53 +0300

    Password Encryption added

--- BootlegEmagService/User/Repository/UserRepository.cs
+++ BootlegEmagService/User/Repository/UserRepository.cs
@@ -20,8 +20,9 @@ namespace BootlegEmagService.User.Repository
         }
 
         //Login
-        public UserModel Find(string name, string password)
+        public UserModel Find(string name, string pass)
         {
+            string password = EncodePasswordToBase64(pass);
             using (var connection = new SQLiteConnection(_connectionString))
             {
                 int count;
@@ -52,11 +53,13 @@ namespace BootlegEmagService.User.Repository
                     return user;
                 }
                 return null;
+                connection.Close();
             }  
         }
 
-        public UserModel Register(string name, string password, string role )
+        public UserModel Register(string name, string pass, string role )
         {
+            string password = EncodePasswordToBase64(pass);
             using (var connection = new SQLiteConnection(_connectionString))
             {
                 connection.Open();
@@ -116,6 +119,34 @@ namespace BootlegEmagService.User.Repository
             }
             return null;
         }
+
+        private static string EncodePasswordToBase64(string password)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[password.Length];
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(password);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        } 
+        private static string DecodeFrom64(string encodedData)
+        {
+            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+            System.Text.Decoder utf8Decode = encoder.GetDecoder();
+            byte[] todecode_byte = Convert.FromBase64String(encodedData);
+            int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+            char[] decoded_char = new char[charCount];
+            utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+            string result = new String(decoded_char);
+
+            return result;
+        }
+
     }
 }
 
